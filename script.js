29b7877fe76c744c42b9563afd5ad0fa6f29f945
David Ghazi <davidaghazi@gmail.com>
2020-03-16 18:58:21 -0400

    Finalized the code. Fixed the save button, text area, and background colours. Added Readme.md file.

--- script.js
+++ script.js
@@ -7,47 +7,53 @@ function getLocalStorage(key) {
     }
 }
 
-function dayPlanner() {
+function dayPlanner(){
     for (let i = 9; i < 18; i++) {
 
-        $('.container').append($(`<div data-time=${i} id='${i}'>`));
-        $('.container').append($('<div class="col-sm-2"> <p class="workHour">' + timeDisplay(i) + '</p>'));
-        $('.container').append($(`<div class="col-sm-8"> <textarea id=text${i} class= userInput placeholder="Add your event here..."></textarea>`));
-        var something = $('.container').append($(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`));
-        getLocalStorage(i)
+        var row = $(`<div data-time=${i} id='${i}' class="row">`);
+        var col1 = $('<div class="col-sm-2"> <p class="workHour">' + timeDisplay(i) + '</p>');
+        var col2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="userInput" placeholder="Add your event here..."></textarea>`);
+        var col3 = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}>SAVE<i class="fas fa-save"></i></button>`);
+
+        row.append(col1);
+        row.append(col2);
+        row.append(col3);
+
+        $(".container").append(row);
+
+        getLocalStorage(i);
     }
-}
+
+    function timeDisplay(hours) {
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        return hours + ':00' + ampm;
+    }
+
+    function completedTasks() {
+        var currentTime = new Date().getHours();
+        for (var i = 9; i < 18; i++) {
+            if ($(`#${i}`).data("time") == currentTime) {
+                $(`#text${i}`).addClass("present");
+            } else if (currentTime < $(`#${i}`).data("time")) {
+                $(`#text${i}`).addClass("future");
+            } else if ($(`#${i}`).data("time") < currentTime) {
+                $(`#text${i}`).addClass("present");
+            }
+        }
+    }
+
+    setInterval(function () {
+        completedTasks();
+    }, 1000);
+
+};
 
 dayPlanner()
 
-function timeDisplay(hours) {
-    var ampm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-    return hours + ':00' + ampm;
-}
-
-function completedTasks() {
-
-    var currentTime = new Date().getHours();
-
-    for (var i = 9; i < 18; i++) {
-        if ($(`#${i}`).data("time") == currentTime) {
-            $(`#text${i}`).addClass("present");
-        } else if ($(`#${i}`).data("time") < currentTime) {
-            $(`#text${i}`).addClass("present");
-        } else if (currentTime < $(`#${i}`).data("time")) {
-            $(`#text${i}`).addClass("future");
-        }
-    }
-}
-
-setInterval(function () {
-    completedTasks();
-}, 1000);
-
-var saveBtn = $('.saveBtn');
-var userInput = $('.userInput')
-saveBtn.on('click', function(){
-    console.log('working')
+$('.saveBtn').on('click', function () {
+    let eventId = $(this).attr('id');
+    let eventText = $(this).parent().siblings().children('.userInput').val();
+    localStorage.setItem(eventId, eventText);
 });
